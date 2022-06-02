ed460beb152ed5f1e40d463d62f1bf61db8a28a4
Shah rahman <toshah.rahman@gmail.com>
2022-06-02 17:48:11 +0300

    webapp with pyramid fun 4

--- pyramid_fun_1/views/default.py
+++ pyramid_fun_1/views/default.py
@@ -1,6 +1,6 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name="home", renderer="pyramid_fun_1:templates/mytemplate.pt")
-def my_view(request):
+@view_config(route_name="home", renderer="pyramid_fun_1:templates/home.pt")
+def index(request):
     return {"project": "pyramid_fun_1"}
