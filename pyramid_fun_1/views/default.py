40974a46515496115907f18ee3dc10a329f809c0
Shah rahman <toshah.rahman@gmail.com>
2022-06-02 18:06:31 +0300

    webapp with pyramid fun 6

--- pyramid_fun_1/views/default.py
+++ pyramid_fun_1/views/default.py
@@ -1,6 +1,6 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name="home", renderer="pyramid_fun_1:templates/home.pt")
-def index(request):
+@view_config(route_name="home", renderer="pyramid_fun_1:templates/home_index.pt")
+def home_index(request):
     return {"project": "python packages index"}
