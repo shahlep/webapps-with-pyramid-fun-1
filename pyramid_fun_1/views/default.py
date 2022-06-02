b1da37be0f324cdf4972d22abc3e9bc2d1d8faea
Shah rahman <toshah.rahman@gmail.com>
2022-06-02 18:13:16 +0300

    webapp with pyramid fun 7

--- pyramid_fun_1/views/default.py
+++ pyramid_fun_1/views/default.py
@@ -1,6 +1,14 @@
 from pyramid.view import view_config
 
 
+def get_test_packages():
+    return [
+        {'name': 'robotframework', 'version': '5.0.0'},
+        {'name': 'requests', 'version': '1.2.3'},
+        {'name': 'playwright', 'version': '1.22'},
+    ]
+
+
 @view_config(route_name="home", renderer="pyramid_fun_1:templates/home_index.pt")
 def home_index(request):
-    return {"project": "python packages index"}
+    return {"project": get_test_packages()}
